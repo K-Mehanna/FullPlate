d5cc567eec4bfe1572e5bfc5b82438942b782de0
Aahil Jouher <aj1722@ic.ac.uk>
2024-05-29 15:39:07 +0100

    feat [AJ]: Add navbar to donor_home_page

--- lib/pages/donor/donor_home_page.dart
+++ lib/pages/donor/donor_home_page.dart
@@ -1,154 +1,45 @@
+import 'package:cibu/pages/donor/donor_dashboard_page.dart';
 import 'package:flutter/material.dart';
-import 'request_detail_page.dart';
-import 'new_request_page.dart';
-
-class RequestItem {
-  final String title;
-  final String location;
-  final String address;
-  final int quantity;
-  final String size;
-  final String status;
-
-  RequestItem({
-    required this.title,
-    required this.location,
-    required this.address,
-    required this.quantity,
-    required this.size,
-    required this.status,
-  });
-}
 
 class DonorHomePage extends StatefulWidget {
-  DonorHomePage({super.key});
+  const DonorHomePage({super.key});
 
   @override
-  DonorHomePageState createState() => DonorHomePageState();
+  State<DonorHomePage> createState() => _DonorHomePageState();
 }
 
-class DonorHomePageState extends State<DonorHomePage> {
-  final List<RequestItem> waitingToLoad = [
-    RequestItem(
-      title: "avocado",
-      location: "Kitchen X",
-      address: "1 Holborn Close, E3 8AB",
-      quantity: 15,
-      size: "M",
-      status: "",
-    ),
-    RequestItem(
-      title: "banana",
-      location: "Kitchen X",
-      address: "1 Holborn Close, E3 8AB",
-      quantity: 10,
-      size: "L",
-      status: "",
-    ),
-    RequestItem(
-      title: "carrot",
-      location: "Kitchen Y",
-      address: "2 Holborn Close, E3 8AC",
-      quantity: 20,
-      size: "S",
-      status: "",
-    ),
-  ];
+class _DonorHomePageState extends State<DonorHomePage> {
+  int _selectedIndex = 0;
 
-  final List<RequestItem> pending = [
-    RequestItem(
-      title: "avocado",
-      location: "",
-      address: "",
-      quantity: 0,
-      size: "N/A",
-      status: "Waiting to be claimed",
-    ),
+  final List<Widget> _pages = [
+    DonorDashboard(),
+    const Placeholder()
   ];
-
-  void addNewRequest(RequestItem item) {
-    setState(() {
-      pending.add(item);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dashboard"),
+      body: SafeArea(
+        child: _pages[_selectedIndex]
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-              child: ListView(
-                children: [
-                  _buildSectionTitle("Waiting to load", waitingToLoad.length),
-                  ...waitingToLoad.map((item) => _buildListItem(context, item)).toList(),
-                  SizedBox(height: 16),
-                  _buildSectionTitle("Pending", pending.length),
-                  ...pending.map((item) => _buildListItem(context, item)).toList(),
-                ],
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: FloatingActionButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => NewRequestPage(
-                        addRequestCallback: addNewRequest,
-                      ),
-                    ),
-                  );
-                },
-                child: Icon(Icons.add),
-              ),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _selectedIndex,
+        destinations: const [
+          NavigationDestination(
             icon: Icon(Icons.dashboard),
-            label: 'Dashboard',
+            label: 'Dashboard'
           ),
-          BottomNavigationBarItem(
+          NavigationDestination(
             icon: Icon(Icons.person),
-            label: 'Profile',
+            label: 'Profile'
           ),
         ],
-        currentIndex: 0,
-        selectedItemColor: Colors.blue,
-        onTap: (index) {},
+        onDestinationSelected: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        }
       ),
-    );
-  }
-
-  Widget _buildSectionTitle(String title, int count) {
-    return Text("$title ($count)", style: TextStyle(fontWeight: FontWeight.bold));
-  }
-
-  Widget _buildListItem(BuildContext context, RequestItem item) {
-    return ListTile(
-      leading: Icon(Icons.person),
-      title: Text("\"${item.title}\""),
-      trailing: Text(item.location.isEmpty ? item.status : item.location),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => RequestDetailPage(item: item),
-          ),
-        );
-      },
     );
   }
 }
-
