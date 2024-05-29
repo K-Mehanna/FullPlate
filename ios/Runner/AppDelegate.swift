13447c9b203777dd2a82e93e8438f38796fd9727
K-Mehanna <k.mehanna25@gmail.com>
2024-05-29 11:39:03 +0100

    config [KM]: Installing dependencies for google maps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDusgS3hbLeajpaVitxr7rEol3AJHmr5-4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
