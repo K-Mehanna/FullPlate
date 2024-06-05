8b4cbc6540afd5f6cc35b1e06b1b09baed77e9fc
Aahil Jouher <aj1722@ic.ac.uk>
2024-06-05 08:46:49 +0100

    feat [AJ]: Create donor and kitchen signup pages

--- lib/pages/auth/donor_signup.dart
+++ lib/pages/auth/donor_signup.dart
@@ -1,10 +1,120 @@
+import 'package:cibu/database/donors_manager.dart';
+import 'package:cibu/models/donor_info.dart';
+import 'package:cibu/pages/donor/donor_home_page.dart';
+import 'package:cibu/widgets/custom_button.dart';
+import 'package:cibu/widgets/custom_text_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 
-class DonorSignupPage extends StatelessWidget {
+import 'package:cibu/pages/auth/address_autocomplete/address_search.dart';
+import 'package:cibu/pages/auth/address_autocomplete/address_service.dart';
+
+
+class DonorSignupPage extends StatefulWidget {
   const DonorSignupPage({super.key});
 
   @override
+  State<DonorSignupPage> createState() => _DonorSignupPageState();
+}
+
+class _DonorSignupPageState extends State<DonorSignupPage> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  
+  final nameController = TextEditingController();
+  final addressController = TextEditingController();
+  final DonorsManager donorsManager = DonorsManager();
+
+  LatLng? location;
+
+  void updateDetailsToFirestore() {
+    DonorInfo donorInfo = DonorInfo(
+      name: nameController.text,
+      location: location!,
+      address: addressController.text,
+      donorId: FirebaseAuth.instance.currentUser!.uid
+    );
+
+    donorsManager.addDonor(donorInfo);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              // Text(
+              //   'Welcome to Cibu! Now for some extra details',
+              //   style: Theme.of(context).textTheme.displayMedium!,
+              // ),
+              // const SizedBox(height: 40),
+      
+              CustomTextField(
+                controller: nameController,
+                hintText: 'Organisation Name',
+                obscureText: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
+              ),
+      
+              const SizedBox(height: 25),
+              CustomTextField(
+                controller: addressController,
+                hintText: 'Address',
+                obscureText: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an address';
+                  }
+                  return null;
+                },
+                onTap: () async {
+                  // generate a new token here
+                  final sessionToken = Uuid().v4();
+      
+                  final Suggestion? result = await showSearch(
+                    context: context,
+                    delegate: AddressSearch(sessionToken),
+                  );
+      
+                  if (result != null) {
+                    final addressCoords = await PlaceApiProvider(sessionToken).getPlaceDetailFromId(result.placeId);
+                    setState(() {
+                      addressController.text = result.description;
+                      location = addressCoords;
+                    });
+                  }
+                }
+              ),
+      
+              const SizedBox(height: 25),
+            
+              CustomButton(
+                onTap: () { 
+                  if (formKey.currentState!.validate() && location != null) {
+                    updateDetailsToFirestore();
+                    Navigator.pushReplacement(
+                      context, 
+                      MaterialPageRoute(
+                        builder: (context) => DonorHomePage()
+                      )
+                    );
+                  }
+                },
+                text: 'Next',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
